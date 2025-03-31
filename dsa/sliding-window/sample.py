38b99ad737a97d219960c04ec9cf660ac502f8f0
uchiha-vivek <vs160125@gmail.com>
2025-03-31 08:28:26 +0530

    ALGORITHMS : sliding-window/max-subarray-sum

---
+++ dsa/sliding-window/sample.py
@@ -0,0 +1,17 @@
+""" Implementing sliding window """
+""" Time complexity : O(N) """
+""" Space complexity : O(1)  """
+class Solution :
+    def max_subarray_sum(self,nums:list[int],k:int):
+        max_sum = float('-inf')
+        window_sum = sum(nums[:k])
+        for i in range(k,len(nums)):
+            window_sum+=nums[i]-nums[i-k]
+            max_sum = max(max_sum,window_sum)
+        return max_sum
+
+sol=Solution()
+nums=[2,1,5,1,3,2]
+k=3 # window size
+ans=sol.max_subarray_sum(nums,k)
+print(ans)
