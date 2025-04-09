55470eb45ede1972566568427f7bf16771eb92df
uchiha-vivek <vs160125@gmail.com>
2025-04-09 19:31:03 +0530

    CODE:Reverse string

--- short-interviews/strings/reverse-string.py
+++ short-interviews/strings/reverse-string.py
@@ -3,6 +3,23 @@
 class Solution:
     def ReverseString(self, s: str) -> str:
         return s[::-1]  # Correctly reverse the input string
+    
+    def scrathReverseString(self,s:str):
+        """ converting list t string takes time compexity of O(N)"""
+        """ .join returns new string of Space complexity of O(N)"""
+        current_str = [ char for char in s ]
+        i=0
+        j=len(s)-1
+        while i < j :
+            temp=current_str[i]
+            current_str[i]=current_str[j]
+            current_str[j]=temp
+            j-=1
+            i+=1
+        return "".join(current_str)
+
+
+
 
 sol = Solution()
 s = 'vivek'
